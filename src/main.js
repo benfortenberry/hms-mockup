1079fb68ed73fe460427450dfc16cd91fca1a56f
Benjamin Fortenberry <benfortenberry@outlook.com>
2020-09-23 12:08:33 -0500

    test

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import uswds from 'uswds';
-import VueMaterial from 'vue-material'
 
 
 Vue.config.productionTip = false
 
 Vue.use(uswds);
-Vue.use(VueMaterial);
 
 
 new Vue({
